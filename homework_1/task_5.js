559b947d03433c5be18f2e78a1567eb90570446b
Irina Shavrina <irinashavrina2003@gmail.com>
2022-01-27 15:23:28 +0200

    minor fixes

--- homework_1/task_5.js
+++ homework_1/task_5.js
@@ -1,6 +1,6 @@
 const regexForValidTime = /([0-1][0-9]|2[0-3])(:)([0-5][0-9])/
 export function getTime(str){
-    let match = str.match(regexForValidTime);
+    const match = str.match(regexForValidTime);
 
     return match?match[0]:"";
 }
