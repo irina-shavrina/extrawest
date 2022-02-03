37f2feb1cd79a332f61a15724c5c86eb81bf9356
Irina Shavrina <irinashavrina2003@gmail.com>
2022-02-03 11:15:41 +0200

    added 16 task

---
+++ homework_2/task_16.js
@@ -0,0 +1,12 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+export const fearNotLetter  = (string) => {
+    const alphabetSlice = alphabet.slice(alphabet.indexOf(string[0]), 
+                        alphabet.indexOf(string[string.length - 1]));
+
+    for(let i = 0; i < alphabetSlice.length; i++){
+        if(!string.includes(alphabetSlice[i])){
+            return alphabetSlice[i];
+        }
+    }
+}
--- homework_2/task_16.test.js
+++ homework_2/task_16.test.js
@@ -1,22 +1,28 @@
 import {fearNotLetter} from './task_16';
 
 describe('fearNotLetter', () => {
-    test('should return  from ',
+    test('should return "d" from "abce"',
         () => {
             const result = fearNotLetter("abce") 	 	 
             expect(result).toBe('d')
         }
     );
-    test('should return  from ',
+    test('should return "i" from "abcdefghjklmno"',
         () => {
             const result = fearNotLetter('abcdefghjklmno')  	 
             expect(result).toBe('i')
         }
     );
-    test('should return  from ',
+    test('should return "u" from "stvwx"',
         () => {
             const result = fearNotLetter('stvwx') 	
             expect(result).toBe('u')
+        }
+    );
+    test('should return undefined from "abcdefghijklmnopqrstuvwxyz"',
+        () => {
+            const result = fearNotLetter('abcdefghijklmnopqrstuvwxyz') 	
+            expect(result).toBe(undefined)
         }
     );
 });
