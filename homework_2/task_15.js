e6fba963fb18d53e6b1bc0d8f24f22aca3f30801
Irina Shavrina <irinashavrina2003@gmail.com>
2022-02-05 15:11:59 +0200

    added 15 task

--- homework_2/task_15.js
+++ homework_2/task_15.js
@@ -1,9 +1,8 @@
 export const squareList = (numberArray) => {
-    return numberArray
-            .reduce((arrayOfNumbers, number) =>{ 
-                if(Math.sign(number) > 0 && Number.isInteger(number)){
-                    return [...arrayOfNumbers, Math.pow(number, 2)];
-                } 
-                return [...arrayOfNumbers];
-                }, [])
+    return numberArray.reduce((arrayOfNumbers, number) =>{ 
+        if(Math.sign(number) > 0 && Number.isInteger(number)){
+            return [...arrayOfNumbers, Math.pow(number, 2)];
+        } 
+        return [...arrayOfNumbers];
+    }, [])
 }
