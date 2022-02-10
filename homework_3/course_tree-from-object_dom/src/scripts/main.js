af36f6b4b308e8bef655f1e50bdfc92f5ce98baf
Irina Shavrina <irinashavrina2003@gmail.com>
2022-02-10 16:31:53 +0200

    added second task

--- homework_3/course_tree-from-object_dom/src/scripts/main.js
+++ homework_3/course_tree-from-object_dom/src/scripts/main.js
@@ -22,7 +22,27 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  
+  for(let item of data){
+    console.log(item);
+    const firstBranchElem = document.createElement('ul');
+    firstBranchElem.classList.add('main-product');
+    firstBranchElem.textContent = item;
+    element.appendChild(firstBranchElem);
+
+    for(let element of item){
+      const secondBranchElem = document.createElement('li');
+      secondBranchElem.classList.add('second-product');
+      secondBranchElem.textContent = element;
+      firstBranchElem.appendChild(secondBranchElem);
+      for(let i of element){
+        const thirdBranchElem = document.createElement('li');
+        thirdBranchElem.classList.add('third-product');
+        thirdBranchElem.textContent = i;
+        secondBranchElem.appendChild(thirdBranchElem);
+      }
+    }
+  }
 }
 
 createTree(tree, food);
