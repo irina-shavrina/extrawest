3c4891f60ee263baadf662ba6672748ae2220345
Irina Shavrina <irinashavrina2003@gmail.com>
2022-02-10 12:15:13 +0200

    first commit

---
+++ homework_3/course_sliding-tree_dom/src/scripts/main.js
@@ -0,0 +1,17 @@
+'use strict';
+const showCategories = () =>{
+    const header = document.getElementsByClassName('item');
+    for(let item of header){
+        item.addEventListener('click', function (){
+            for(let element of item.children){
+                if(element.hidden === false){
+                    element.hidden = true;
+                }
+                else{
+                    element.hidden = false;
+                }
+            }
+        })
+    }
+}
+showCategories();
